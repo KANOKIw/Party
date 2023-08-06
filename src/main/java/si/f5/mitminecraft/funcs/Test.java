5b090f4c202174e9ffcb13d1297154f278d2953d
KANOKIw <123856636+KANOKIw@users.noreply.github.com>
2023-08-06 22:07:20 +0900

    Add files via upload

---
+++ src/main/java/si/f5/mitminecraft/funcs/Test.java
@@ -0,0 +1,30 @@
+package si.f5.mitminecraft.funcs;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args){
+        List<String> v = new ArrayList<String>();
+        String[] a = {"a"};
+        String sentenceJson = "src\\main\\java\\si\\f5\\mitminecraft\\funcs\\sentences.json";
+        double vc = Float.valueOf(30)/Float.valueOf(60);
+        System.out.println(ChatColor.WHITE+"a");
+        
+    }
+}
